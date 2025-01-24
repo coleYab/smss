d4bd25930dd598360b0c1f78996b1777771bd587
DanielJohn17 <danielyohannes38@gmail.com>
2025-01-24 22:12:39 +0300

    Upload grade

--- frontend/app/student/@submit_grade/page.jsx
+++ frontend/app/student/@submit_grade/page.jsx
@@ -1,5 +1,30 @@
+import { Box, Container, Typography } from "@mui/material";
+// import { UploadButton } from "../../../components/uploadthing";
+
 const SubmitGrade = () => {
-  return <div>Submit Grade</div>;
+  return (
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Typography variant="h2">Submit School Grade</Typography>
+      <Typography variant="h6" sx={{ mt: 4 }}>
+        Submit Your School Grade to the School Admin
+      </Typography>
+
+      <Box>
+        {/* <UploadButton
+          endpoint="imageUploader"
+          onClientUploadComplete={(res) => {
+            // Do something with the response
+            console.log("Files: ", res);
+            alert("Upload Completed");
+          }}
+          onUploadError={(error) => {
+            // Do something with the error.
+            alert(`ERROR! ${error.message}`);
+          }}
+        /> */}
+      </Box>
+    </Container>
+  );
 };
 
 export default SubmitGrade;
